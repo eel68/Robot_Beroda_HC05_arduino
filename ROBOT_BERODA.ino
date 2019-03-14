7be8ad42c2669667be5e8afc59a89a82bd4c753a
eel68 <48557363+eel68@users.noreply.github.com>
2019-03-15 04:16:37 +0700

    Add files via upload

---
+++ ROBOT_BERODA.ino
@@ -0,0 +1,55 @@
+int dataku=0;
+int dataku1=0;
+
+void setup() {
+ Serial.begin(9600);
+ pinMode(9,OUTPUT);
+ pinMode(10,OUTPUT);
+ pinMode(11,OUTPUT);
+ pinMode(12,OUTPUT);
+
+ digitalWrite(9,HIGH);
+ digitalWrite(10,HIGH);
+ digitalWrite(11,HIGH);
+ digitalWrite(12,HIGH);
+}
+
+void loop() {
+if(Serial.available() > 0){
+      dataku = Serial.read();
+      if (dataku=='A') {
+        digitalWrite(9,HIGH);
+        digitalWrite(10,LOW);
+        digitalWrite(11,HIGH);
+        digitalWrite(12,LOW);
+      }
+
+       if (dataku=='B') {
+        digitalWrite(9,LOW);
+        digitalWrite(10,HIGH);
+        digitalWrite(11,LOW);
+        digitalWrite(12,HIGH);
+      }
+      
+      if (dataku=='C') {
+        digitalWrite(9,LOW);
+        digitalWrite(10,HIGH);
+        digitalWrite(11,HIGH);
+        digitalWrite(12,LOW);
+      }
+      
+      if (dataku=='D') {
+        digitalWrite(9,HIGH);
+        digitalWrite(10,LOW);
+        digitalWrite(11,LOW);
+        digitalWrite(12,HIGH);
+      }    
+        
+      if (dataku=='E') {
+        digitalWrite(9,HIGH);
+        digitalWrite(10,HIGH);
+        digitalWrite(11,HIGH);
+        digitalWrite(12,HIGH);
+      }
+    }
+}
